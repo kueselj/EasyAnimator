8878493dc55d6310e10fed0c501899e30140c9c9
Math-ias <mathi4semail@gmail.com>
2020-03-24 11:45:48 -0400

    Make model fields private following feedback on handins server.
    "Fields/instance variables should be set to private/protected. If they are not,
    please document why so."

--- src/cs3500/easyanimator/model/EasyAnimator.java
+++ src/cs3500/easyanimator/model/EasyAnimator.java
@@ -21,13 +21,14 @@ import java.util.Optional;
 */
 public class EasyAnimator implements IAnimatorModel {
 
-  HashMap<String, IShape> shapes;
+  // These fields were made private following assignment 5 feedback.
+  private HashMap<String, IShape> shapes;
   // The list of motions we associate with will have the invariant that the end state of the
   // previous motion will match the start state of the next motion.
   // Additionally none of the motions will clash in tick range.
   // We have another invariant here that the keysets for the shapes map and the motions map should
   // match.
-  HashMap<String, List<IMotion>> motions;
+  private HashMap<String, List<IMotion>> motions;
 
   /**
    * Basic constructor for an EasyAnimator, just gives sets both fields to empty HashMaps.
